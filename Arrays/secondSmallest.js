a0a598f19f6f0ef1b34f7f295e7719d73e51214b
Stewin K Wilson <stewinkurissingal@gmail.com>
2025-03-07 17:31:17 +0530

    Second smallest

---
+++ Arrays/secondSmallest.js
@@ -0,0 +1,18 @@
+// Find the second smallest element from an array
+//The time complexity for this algorithm is O(n) and the space complexitey is O(1)
+let arr = [12,43,65,12,87,45,33]
+
+function secondSmallest(arr){
+    let smallest = Infinity
+    let secondSmallest = Infinity
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]<smallest){
+            secondSmallest = smallest
+            smallest = arr[i]
+        }else if(arr[i]>smallest && arr[i]<secondSmallest){
+            secondSmallest = arr[i]
+        }
+    }
+    return secondSmallest
+}
+console.log(secondSmallest(arr))
