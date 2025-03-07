cd6414ca0bcdaac42f1ef672aacd67c3d3e69442
Stewin K Wilson <stewinkurissingal@gmail.com>
2025-03-07 17:25:43 +0530

    Second Largest

--- Arrays/arrayDataStructure.js
+++ Arrays/arrayDataStructure.js
@@ -1,4 +1,4 @@
-//Splice
+//1. Splice
 //In JavaScript, the splice() method is used to add, remove, 
 // or replace elements in an array. 
 // It modifies the original array and returns an array of removed elements.
@@ -13,3 +13,17 @@ function removeElement(array,index){
     return array
 }
 console.log(removeElement(arr,5))
+
+//2. Slice
+//In JavaScript, the slice() method is used to extract a portion of an array without modifying the original array.
+//It returns a shallow copy of the selected elements.
+// array.slice(start, end);
+//start – (Optional) The index where extraction begins. If omitted, it starts from index 0.
+//end – (Optional) The index where extraction stops (not included in the result). If omitted, it extracts until the end of the array.
+
+let arr2 = [1,2,3,4,5,6,7,8,9,10]
+function extract(array,start,end){
+    let sliced = array.slice(start,end)
+    return sliced
+}
+console.log(extract(arr2,1,6))
---
+++ Arrays/secondLargest.js
@@ -0,0 +1,18 @@
+// Find the second largest element from an array
+//The time complexity for this algorithm is O(n) and the space complexitey is O(1)
+let arr = [12,43,65,12,87,45,33]
+
+function secondLargest(array){
+    let largest = -Infinity 
+    let secondLargest = -Infinity
+    for(let i=0;i<array.length;i++){
+        if(array[i]>largest){
+            secondLargest = largest
+            largest = array[i]
+        }else if(array[i]<largest && array[i]>secondLargest){
+            secondLargest = array[i]
+        }
+    }
+    return secondLargest
+}
+console.log(secondLargest(arr))
