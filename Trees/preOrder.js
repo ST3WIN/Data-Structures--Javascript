7681f2b63559e4d4fd5b364d5f9c70f2834e5ebb
Stewin K Wilson <stewinkurissingal@gmail.com>
2025-04-21 12:11:53 +0530

    BST preOrder

---
+++ Trees/preOrder.js
@@ -0,0 +1,71 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+class BinarySearchTree{
+    constructor(){
+        this.root = null
+    }
+    isEmpty(){
+        return this.root === null
+    }
+    insert(value){
+        const newNode = new Node(value)
+        if(this.isEmpty()){
+            this.root = newNode
+        }else{
+            this.insertNode(this.root,newNode)
+        }
+    }
+    insertNode(root , newNode){
+        if(newNode.value < root.value){
+            if(root.left === null){
+                root.left = newNode
+            }else{
+                this.insertNode(root.left,newNode)
+            }
+        }else{
+            if(root.right === null){
+                root.right = newNode
+            }else{
+                this.insertNode(root.right , newNode)
+            }
+        }
+    }
+    search(root , value){
+        if(!root){
+            return false
+        }else{
+            if(root.value === value){
+                return true
+            }else if(value < root.value){
+               return this.search(root.left , value) 
+            }else{
+                return this.search(root.right , value)
+            }
+        }
+    }
+    preOrder(root){
+        //DFS tree traversal 
+        // read the node value , visit the left subtree and read,
+        //after the left subtree is over visit the right sub tree and read the value
+        if(root){
+            console.log(root.value)
+            this.preOrder(root.left)
+            this.preOrder(root.right)
+        }
+    }
+}
+let bst = new BinarySearchTree()
+bst.insert(10)
+bst.insert(5)
+bst.insert(15)
+bst.insert(3)
+bst.insert(7)
+console.log(bst.search(bst.root , 10))
+console.log(bst.search(bst.root , 5))
+console.log(bst.search(bst.root , 15))
+bst.preOrder(bst.root)
